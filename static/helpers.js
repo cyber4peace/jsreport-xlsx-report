0bab7a819041c777ce942166cfe736e488dd80d4
cyber4peace <LonelyAngel@mail.ru>
2022-03-15 09:46:38 +0600

    Union supports tables and simple attributes

--- static/helpers.js
+++ static/helpers.js
@@ -1,22 +1,4 @@
 function xlsxTable (context, options) {
-  const Handlebars = require('handlebars');
-
-  const ret = [];
-  let data;
-  if (options.data) {
-    data = Handlebars.createFrame(options.data);
-  }
-  if (data && Array.isArray(context)) {
-    for (let i = 0; i < context.length; i++) {
-      if (i in context) {
-        data.key = i;
-        data.index = i;
-        data.first = i === 0;
-        data.last = (i === (data.length - 1));
-      }
-      ret.push(options.fn(context[i], { data: data }));
-    }
-  }
-
-  return new Handlebars.SafeString(ret.join('$$$xlsxRow$$$'));
+  const Handlebars = require('handlebars')
+  return Handlebars.helpers.each(context, options)
 }
